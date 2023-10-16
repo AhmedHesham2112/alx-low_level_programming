6db4cc548bf8ecfcf04d325f3187bd4cbf775f58
AhmedHesham2112 <ahmed.hesham.khattab97@gmail.com>
2023-10-16 09:47:53 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @n: input value.
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (1)
+	{
+		if (str[i] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[i]);
+		i++;
+	}
+}
