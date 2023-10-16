cebd35bf8015d9cfea7bb444be3381a60eec781a
AhmedHesham2112 <ahmed.hesham.khattab97@gmail.com>
2023-10-16 10:01:08 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a reverse string, followed by a new line, to stdout.
+ * @s: input value.
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (1)
+	{
+		if (s[i] == '\0')
+			break;
+		i++;
+	}
+	i--;
+	while (1)
+	{
+		if (i == 0)
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(s[i]);
+		i--;
+	}
+}
