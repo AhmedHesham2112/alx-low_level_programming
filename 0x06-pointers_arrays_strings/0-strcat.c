0ce632e590bbfe84161f50cb5d6b76247e235faf
AhmedHesham2112 <ahmed.hesham.khattab97@gmail.com>
2023-10-18 05:05:20 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,22 +14,10 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 
 	while (src[srclen] != '\0')
-	{
 		srclen++;
-	}
 	while (dest[destlen] != '\0')
-	{
 		destlen++;
-	}
 	for (i = destlen; i <= (destlen + srclen); i++)
-	{
-		if (i == (destlen + srclen))
-		{
-			dest[i] = '\0';
-			break;
-		}
-		else
-			dest[i] = src[i - destlen];
-	}
+		dest[i] = src[i - destlen];
 	return (dest);
 }
