c402e3174c1b5d466ffd259dc2bf47ac134270f8
AhmedHesham2112 <ahmed.hesham.khattab97@gmail.com>
2023-10-23 12:27:54 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,11 +11,11 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	for (i = 0; haystack[i] >= '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		if (haystack[i] == needle[0])
 		{
-			for (j = 0; needle[j] >= '\0'; j++)
+			for (j = 0; needle[j] != '\0'; j++)
 			{
 				if (haystack[i + j] != needle[j])
 					break;
