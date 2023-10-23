7ef49d3d7cafa9d644ca803db5ef6d49e0c53352
AhmedHesham2112 <ahmed.hesham.khattab97@gmail.com>
2023-10-23 14:48:24 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -17,14 +17,12 @@ char *_strstr(char *haystack, char *needle)
 		{
 			for (j = 0; needle[j] != '\0'; j++)
 			{
-				if (haystack[i + j] == needle[j])
-					continue;
-				else
+				if (haystack[i + j] != needle[j])
 					break;
 			}
+		}
 			if (needle[j] == '\0')
 				return (haystack + i);
-		}
 	}
 	return ('\0');
 }
