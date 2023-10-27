9e0ebaae98eb3e1a16655e625fb88e393c01411d
root <root@DESKTOP-1N3TEFC>
2023-10-27 18:02:02 +0200

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+		printf("Error\n");
+	else
+	{
+		int mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	return (0);
+}
