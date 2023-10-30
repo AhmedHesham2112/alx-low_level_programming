5f7224b96031bfae25a2a9570c9bcad675dcdb4b
root <root@DESKTOP-1N3TEFC>
2023-10-31 00:06:27 +0200

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,6 +28,15 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
