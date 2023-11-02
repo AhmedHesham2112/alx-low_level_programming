0c08b985bd32afccec6b627c7e13a0df8b40d121
root <root@DESKTOP-1N3TEFC>
2023-11-02 18:57:56 +0200

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0, j = 0, k = 0;
 	char *c;
 
-	if (s1 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 	if (s1 != NULL)
 	{
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	if (j > n)
 	{
-		c = malloc(sizeof(char) * (i + n + 1));
+		c = malloc(sizeof(char) * (i + n));
 		if (c == NULL)
 		{
 			return (NULL);
@@ -73,7 +73,7 @@ char *strcon2(char *s1, char *s2, unsigned int n)
 			i++;
 		}
 	}
-	c = malloc(sizeof(char) * (i + n + 1));
+	c = malloc(sizeof(char) * (i + n));
 	if (c == NULL)
 	{
 		return (NULL);
