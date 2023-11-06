91394a91013dac9a2923f507c0feaeac8b02570d
root <root@DESKTOP-1N3TEFC>
2023-11-06 17:38:29 +0200

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,52 +4,6 @@
 #include "dog.h"
 
 /**
- * _strlen - returns the length of a string.
- * @s: input string.
- * Return: Always 0.
- */
-
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (1)
-	{
-		if (s[i] == '\0')
-			break;
-		i++;
-	}
-	return (i);
-}
-
-/**
- * _strcpy - copies the string pointed to by src
- * @dest: the string.
- * @src: input text.
- * Return: Always 0.
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	int i, j;
-
-	i = 0;
-
-	while (src[i] != '\0')
-	{
-		i++;
-	}
-
-	for (j = 0; j < i; j++)
-	{
-		dest[j] = src[j];
-	}
-	dest[j] = '\0';
-
-	return (dest);
-}
-
-/**
  * new_dog -  initialize a variable of type struct dog
  * @name: name to initialize
  * @age: age to initialize
@@ -66,8 +20,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	lth_n = _strlen(name);
-	lth_o = _strlen(owner);
+	lth_n = strlen(name);
+	lth_o = strlen(owner);
 	dog->name = malloc(lth_n + 1);
 	dog->owner = malloc(lth_o + 1);
 	if (dog->name == NULL || dog->owner == NULL)
@@ -77,8 +31,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-	_strcpy(dog->name, name);
-	_strcpy(dog->owner, owner);
+	strcpy(dog->name, name);
+	strcpy(dog->owner, owner);
 	dog->age = age;
 	return (dog);
 }
