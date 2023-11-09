4f25bfda96aa11ea169425b2e80b979cceb2f550
root <root@DESKTOP-1N3TEFC>
2023-11-09 16:25:47 +0200

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - prints a name.
+ * @n: the first number to sum
+ *
+ * Return: the sum of all the numbers.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int result = 0;
+	unsigned int i = 0;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		int value = va_arg(args, int);
+
+		result += value;
+	}
+
+	va_end(args);
+	return (result);
+}
