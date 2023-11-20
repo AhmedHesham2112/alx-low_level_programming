be8873dd23c498bb7d851fcee79c5c853acd18fd
root <root@DESKTOP-1N3TEFC>
2023-11-20 22:02:33 +0200

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,11 +8,15 @@
 
 int pop_listint(listint_t **head)
 {
+	listint_t *temp = malloc(sizeof(listint_t));
 	int new_n;
 
 	if (!head)
 		return (0);
 	new_n = (*head)->n;
+	temp = (*head)->next;
 	free(*head);
+	*head = temp;
+	free(temp);
 	return (new_n);
 }
