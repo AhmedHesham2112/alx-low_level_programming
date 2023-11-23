5981169c75fb38eba006870dfe37bfbed616e6bd
root <root@DESKTOP-1N3TEFC>
2023-11-23 16:06:36 +0200

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,11 +10,12 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i = 0, len = strlen(b), num = 0;
+	unsigned int i = 0, len = 0, num = 0;
 
 	if (b == NULL || b[0] == '\0')
 		return (0);
-
+	while (b[len])
+		len++;
 	for (i = 0; i < len; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
