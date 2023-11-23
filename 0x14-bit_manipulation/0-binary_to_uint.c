8a03fab5e04ece08614ef254356f8e5feafa7320
root <root@DESKTOP-1N3TEFC>
2023-11-23 15:13:11 +0200

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <math.h>
 #include <string.h>
 
 /**
@@ -11,7 +10,7 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i = 0, count = 1, len = strlen(b), j = 0, num = 0;
+	unsigned int i = 0, len = strlen(b), num = 0;
 
 	if (b == NULL)
 		return (0);
@@ -20,18 +19,8 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		if (b[i] == '1')
-		{
-			if (i == len - 1)
-				num += 1;
-			else
-			{
-				for (j = 1; j < len - i; j++)
-					count = count * 2;
-				num += count;
-				count = 1;
-			}
-		}
+
+		num = (num << 1) + (b[i] - '0');
 	}
 
 	return (num);
