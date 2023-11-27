6212ff3088aaff123a5e7143af3f43fdf596fce8
root <root@DESKTOP-1N3TEFC>
2023-11-27 19:37:23 +0200

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	o = open(filename, O_APPEND | O_RDWR, 0664);
 	w = write(o, text_content, len);
-	
+
 	if (o < 0 || w < 0)
 		return (-1);
 	close(o);
