dd0c82ab3c264b09c7cf221ae39ef5865bbe5d7c
root <root@DESKTOP-1N3TEFC>
2024-01-12 04:13:29 +0200

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,41 +10,39 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = NULL, *temp = NULL;
-	unsigned int i = 0;
+	dlistint_t *new_node;
+	dlistint_t *temp;
+	unsigned int i;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (NULL);
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	new_node->n = n;
-	if (!h || !(*h))
-		*h = new_node;
+	new_node = NULL;
+	if (idx == 0)
+		new_node = add_dnodeint(h, n);
 	else
 	{
 		temp = *h;
-		while (idx != i++ && temp->next)
+		i = 1;
+		while (temp != NULL)
+		{
+			if (i == idx)
+			{
+				if (temp->next == NULL)
+					new_node = add_dnodeint_end(h, n);
+				else
+				{
+					new_node = malloc(sizeof(dlistint_t));
+					if (new_node != NULL)
+					{
+						new_node->n = n;
+						new_node->next = temp->next;
+						new_node->prev = temp;
+						temp->next->prev = new_node;
+						temp->next = new_node;
+					}
+				}
+				break;
+			}
 			temp = temp->next;
-		if (temp->next)
-			new_node->prev = temp->prev;
-		else
-			new_node->prev = temp;
-		if (idx == i)
-			temp->next = new_node, new_node->prev = temp;
-		else if (idx == i - 1)
-		{
-			if (temp->prev)
-				temp->prev->next = new_node;
-			else
-				*h = new_node;
-			temp->prev = new_node;
-			new_node->next = temp;
-		}
-		else
-		{
-			free(new_node);
-			return (NULL);
+			i++;
 		}
 	}
 	return (new_node);
